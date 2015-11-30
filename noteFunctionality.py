7bf4b7fce1cc0577cb54b0d12758ccbafcf72962
JGT <john.thevos@gmail.com>
2015-11-30 11:48:05 -0500

    updated noteFunctionality

--- noteFunctionality.py
+++ noteFunctionality.py
@@ -2,8 +2,15 @@
 from random import *
 from gui import *
 
+phrase_Array = []
+global previousState
+
+def smartUndo(eventList):
+   eventList[0]
 # event = mouseRelease
 # y_position is the vertical position of the new dragged note
+# y can be both positive and negative to indicate increasing
+# and decreasing pitch
 def changeNote(note,y_position):
    startingPitch = note.getPitch()
    newPitch = note.setPitch(startingPitch + y_position)
@@ -17,5 +24,23 @@ def duplicateNote(note):
 def changeNoteDuration(note,rthm_value):
    return note.setDuration(rthm_value)
 
+# not sure if I need to be utilizing self
+# for the scale of editing we're doing, I don't think 
+# we'll be utilizing the part level until the file is finished
+# i.e., it appends all the phrases once "save" is hit
+def addNote(pitch,duration,phrase):
+   previousState = phrase
+   phrase2 = phrase.copy()
+   note = Note(pitch, duration)
+   self.phrase2.addNote(note)
+   #phrase_Array.
 
+def preSaveHousekeeping():
+   
+   
+   
+def saveState():
+   preSaveHousekeeping()
+   
+   workingScore = 0
 
