ddf0748caeed7622f9bc03ae9e7cda3ee4af5f8e
JGT <john.thevos@gmail.com>
2015-12-06 14:57:38 -0500

    starting GUI with dimensions matching Justin's image

--- MVC_jamWithFriends.py
+++ MVC_jamWithFriends.py
@@ -1,3 +1,25 @@
 ﻿from gui import *
+from controller import *
 
-d = Diplay("simple display", 800, 1000)
+d = Display("simple display", 1280, 720)
+
+menu = Menu("File")
+menuItemList = ["New Jamspace","Open Jamspace","Save Jamspace","Connect Device to Computer","Browse Jamspaces"]
+menuFunctionList = [createJamspace,openJamspace,saveJamspace,connectToOSC,browseJams]
+
+menu.addItemList(menuItemList, menuFunctionList)
+d.addMenu(menu)
+
+def sendDisplayToController(display):
+   return display
+
+slider1 = Slider(VERTICAL, 0, 127, 50, changeVolume) 
+
+#button1 = Button("Play music", playMusic) 
+#button2 = Button("Pause music", pauseMusic)
+
+   
+#ddl1 = DropDownList(["Piano", "Guitar", "Drums", "Bass","Gunshot"], itemSelected)
+
+
+      
