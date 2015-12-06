e374d0af426783ff670a46bf83b391f506de68c2
JGT <john.thevos@gmail.com>
2015-12-06 15:14:35 -0500

    new controller functionality

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 ﻿from MVC_jamWithFriends import *
 from music import *
+from gui import *
 
 # Basic addition of parts based on instrument. 
 # Only 5 are included as of to serve as the MVP
@@ -32,7 +33,11 @@ def connectToOSC(ipAddrComp,ipAddrPhone):
 
 def browseJams():
    print "Coming soon!"
-   
+
+# delete functions for different levels of granularity 
+def deleteSubPhrase(phr):
+   print phr.getTitle() + " has been deleted."    
+     
 def deleteSubPart(part):
    print part.getTitle() + "has been deleted."
    part.empty()
@@ -44,6 +49,9 @@ def copyPart(part, startTime):
 
 # Should the composer decide to change the instrument of an existing part, 
 # they can do so with this.   
+
+def restartTrack():
+   
 def changeInstrument(part,new_instrument):
    part.setInstrument(new_instrument)
 
@@ -57,16 +65,24 @@ def playPause(material):
 # need to add actual volume control here   
 def changeVolume(new_value):
    slider1.setValue(new_value)
-   
+
+# takes a default score in the root file directory 
+# clones it, and the cloned file will be what is manipulated throughout
+# returns the new score       
 def readNewMIDISong():
    score = Score()              # create an empty score
    Read.midi(score, "song.mid") # read MIDI file into it
    score2 = score.copy()
    
    return score2
+
 # flips between major and minor 
 def changeQuality(quality):
-   score.setKeyQuality(quality)
+   if score.getKeyQuality() == 0:
+      score.setKeyQualty(1)
+   elif score.getKeyQuality() == 1:
+      score.setKeyQuality(0)
+      
    return score
 
 # possibly link this to a slider that goes from -7 to 7 which will
