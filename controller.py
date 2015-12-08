81b1f1ce881d7a73e426d1b60028dc7188967900
JGT <john.thevos@gmail.com>
2015-12-08 12:32:25 -0500

    updated controller and model

--- controller.py
+++ controller.py
@@ -1,19 +1,14 @@
 ﻿import view, model
+from timer
 
-# Basic addition of parts based on instrument. 
-# Only 5 are included as of to serve as the MVP
-# Channel restrictions are observed.
-def itemSelected(argument):
-   if argument == "Piano":
-      part = Part("Piano Part",PIANO,0)
-   elif argument == "Guitar":
-      part = Part("Guitar Part", GUITAR, 1)
-   elif argument == "Bass":
-      part = Part("Bass Part", GUITAR, 2)
-   elif argument == "Gunshot":
-      part = Part("Gun Part", GUNSHOT, 3)
-   elif argument == "Drums":
-      part = Part("Percussion Part", STEEL_DRUMS, 9)
+def importSong():
+   model.readNewMIDISong()
+
+def getCurrentScore():
+   return model.score
+
+def viewInstrumentParts(): # used to view all parts currently in the score
+   model.viewParts()
 
 # unsure as to whether or not the display needs to be passed
 # has been included for now
@@ -32,9 +27,6 @@ def connectToOSC(ipAddrComp,ipAddrPhone):
 def browseJams():
    print "Coming soon!"
    
-def createPart():
-   part = Part()
-   return part
 
 # delete functions for different levels of granularity 
 def deleteSubPhrase(part, phr):
@@ -47,9 +39,7 @@ def deleteSubPart(score,part):
    return score
 
 # to be called for the loop button   
-def copyPart(part):
-   Mod.repeat(part,1)
-   return part
+
 
 def copyPhrase(phr):
    Mod.repeat(phr,1)
@@ -69,7 +59,8 @@ def changeInstrument(part,new_instrument):
    part.setInstrument(new_instrument)
 
 # Alternates between play and pause depending on which is active      
-def playPause(material):
+def playPause():
+   material = model.score
    if material.isPlaying():
       matieral.pause()
    elif not material.isPlaying():
@@ -79,24 +70,8 @@ def playPause(material):
 def changeVolume(new_value):
    slider1.setValue(new_value)
 
-# takes a default score in the root file directory 
-# clones it, and the cloned file will be what is manipulated throughout
-# returns the new score       
-def readNewMIDISong():
-   score = Score()              # create an empty score
-   Read.midi(score, "song.mid") # read MIDI file into it
-   score2 = score.copy()
-   
-   return score2
 
-# flips between major and minor 
-def changeQuality(quality):
-   if score.getKeyQuality() == 0:
-      score.setKeyQualty(1)
-   elif score.getKeyQuality() == 1:
-      score.setKeyQuality(0)
-      
-   return score
+
 
 # possibly link this to a slider that goes from -7 to 7 which will
 # correspond to flats and sharps with 0 being C in major and A in minor
--- model.py
+++ model.py
@@ -1,2 +1,47 @@
 ﻿import view, controller
+from music import *
 
+global score = Score() # create empty score
+
+# takes a default score in the root file directory 
+# clones it, and the cloned file will be what is manipulated throughout
+# returns the new score       
+def readNewMIDISong():
+   score = Read.midi(score, "song.mid") # read MIDI file into it   
+   return score
+
+# Basic addition of parts based on instrument. 
+# Only 5 are included as of to serve as the MVP
+# Channel restrictions are observed.
+def addPart(argument):
+   if argument == "Piano":
+      part = Part("Piano Part",PIANO,0)
+   elif argument == "Guitar":
+      part = Part("Guitar Part", GUITAR, 1)
+   elif argument == "Bass":
+      part = Part("Bass Part", GUITAR, 2)
+   elif argument == "Gunshot":
+      part = Part("Gun Part", GUNSHOT, 3)
+   elif argument == "Drums":
+      part = Part("Percussion Part", STEEL_DRUMS, 9)
+
+   return part
+
+def viewParts():
+   return score.getPartList()
+
+def viewPhrases():
+   part.getPhraseList()
+
+def copyPart(part):
+   Mod.repeat(part,1)
+   return part
+
+# flips between major and minor 
+def changeQuality(quality):
+   if score.getKeyQuality() == 0:
+      score.setKeyQualty(1)
+   elif score.getKeyQuality() == 1:
+      score.setKeyQuality(0)
+      
+   return score
